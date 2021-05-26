4611c892b12672b01cf541a4da239fa254864a07
ArcticTheRogue <arctictherogue@gmail.com>
2021-05-26 15:12:07 -0400

    Get basic bot set up

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mythical-linux/mythical-telegram-bot
 
 go 1.16
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.0.0-rc1 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
 	fmt.Println("Hello, world!")
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_APITOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on accout %s", bot.Self.UserName)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		if update.Message.IsCommand() {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+
+		switch update.Message.Command() {
+		case "about":
+			msg.Text = "Mythical Bot is a Telegram bot designed for the Mythical Linux Telegram room"
+		}
+		if _, err := bot.Send(msg); err != nil {
+			log.Panic(err)
+		}
+	}
 }
